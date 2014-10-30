0b589e203d8fe86c7076506d6ceb23fac5b25a16
aharenchie <e125731@ie.u-ryukyu.ac.jp>
2014-10-30 14:03:10 +0900

    first commit

---
+++ src/auto.java
@@ -0,0 +1,48 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.image.*;
+
+public class auto extends Applet{
+
+	Image img;
+	int w = 400;
+	int h = 400;
+	
+	int pix[] = new int[w*h];
+	IndexColorModel cm;
+	
+	public void init(){
+		byte[] r = {(byte)0x99,(byte)0x00};
+		byte[] g = {(byte)0x99,(byte)0x00};
+		byte[] b = {(byte)0x99,(byte)0xff};
+		
+		cm = new IndexColorModel(8,2,r,g,b);
+		
+		setPix();
+	}
+
+	public void setPix() {
+		pix[w/2] = 1;
+		for(int y=1;y<h;y++){
+			for(int x=1;x<w;x++){
+				int p = y*w+x;
+				pix[p] = pix[p-w-1]+pix[p-w+1];
+				if(pix[p] == 1){
+					pix[p] = 1;
+				}else{
+					pix[p] = 0;
+				}
+			}	
+		}
+		
+	}
+
+	public void paint(Graphics g){
+		MemoryImageSource mimg = new MemoryImageSource(w,h,cm,pix,0,w);
+		
+		img = createImage(mimg);
+		
+		g.drawImage(img, 0, 0, this);
+		
+	}
+}
